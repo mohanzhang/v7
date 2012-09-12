c1a1d7e5544e437f0e7a58cbb3bab84e988c9d79
Mohan Zhang <mohanzhang@gmail.com>
2012-09-12 00:46:30 -0500

    Scaffold site layout

--- Site.hs
+++ Site.hs
@@ -1,10 +1,34 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main (main) where
 
+import Control.Arrow
+
 import Hakyll
 
 main = hakyllWith config $ do
-  return ()
+  -- twitter bootstrap
+  match "bootstrap-img/*" $ do
+    route idRoute
+    compile copyFileCompiler
+
+  -- assets
+  match "javascripts/*" $ do
+    route idRoute
+    compile copyFileCompiler
+
+  match "index.haml" $ do
+    route $ setExtension "html"
+    compile haml
+
+  match "style.sass" $ do
+    route $ setExtension "css"
+    compile sass
+
+sass :: Compiler Resource String
+sass = getResourceString >>> unixFilter "sass" ["-s"] >>> arr compressCss
+
+haml :: Compiler Resource String
+haml = getResourceString >>> unixFilter "haml" ["-s", "-r", "coffee-filter"]
 
 config :: HakyllConfiguration
 config = defaultHakyllConfiguration
